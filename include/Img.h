03ca200523eb9ba2a1bbdd8cb5e3c1b448f6f4b0
wanzzhehe <wanzzmicro@outlook.com>
2022-01-10 23:14:54 +0800

    更新图像管理器（还有个编译错误QwQ）

--- include/Img.h
+++ include/Img.h
@@ -6,46 +6,67 @@
 #include <memory>
 
 namespace book {
-    namespace fs = std::filesystem;
+    namespace fs = std::filesystem; // 给 std::filesystem 起个别名
 
     constexpr std::array<std::string_view, 4> IMG_TYPES = {
-        ".jpg", ".png", ".gif", ".webp"
-    };
+        ".jpg", ".png", ".gif", ".webp" };      // 合法的图像文件后缀
 
     class ImagesManager;
 
     class ImagesManager final {
     public:
+        // 默认构造函数
         ImagesManager() = default;
-        ImagesManager(const fs::path& path);
-        ImagesManager(const fs::path& srcPath, const fs::path& destPath, bool removeOldFile = false, bool removeOldDirectory = false);
-        ImagesManager(const ImagesManager&) = delete;
-        ImagesManager(ImagesManager&& man);
+        // 将 path 下所有图像文件都归入管理器
+        ImagesManager(const fs::path &path);
+        // 使用 images 内储存的所有的图像路径初始化管理器
+        ImagesManager(const std::vector<fs::path> &images);
+        // 使用 images 内储存的所有的图像路径初始化管理器
+        ImagesManager(std::vector<fs::path> &&images);
+        // 禁用复制构造
+        ImagesManager(const ImagesManager &) = delete;
+        // 移动构造函数
+        ImagesManager(ImagesManager &&man);
 
     private:
-        fs::path m_curDir;
-        std::vector<fs::path> m_imagesList;
+        std::vector<fs::path> m_images;         // 图像路径
 
     public:
-        /* 文件管理部分 */
-
-        void move(const fs::path& destPath, bool removeOldDirectory = false);
-        void copy(const fs::path& destPath);
-        void remove();
-        void remove(std::size_t index);
-        void rename(std::size_t index, std::string_view newName);
-        void chdir(const fs::path &destPath);
-        void flush();
+        // 将当前所有图像文件复制到 destPath 目录下
+        // destPath 必须为目录
+        // moveOldPath 为 true 时，更新管理器储存的图像路径到新路径
+        void copy(const fs::path &destPath, bool moveOldPath = false);
+        // 将当前所有图像文件移动到 destPath 目录下
+        // destPath 必须为目录
+        // 同时更新管理器储存的图像路径
+        void move(const fs::path &destPath);
+        // 清空管理器
+        void clear(bool removeFiles = false);
+        // 将第 index 个图像移除管理器（编号从 0 开始）
+        // 当 removeFile 为 true 时，将文件从磁盘上删除
+        void remove(std::size_t index, bool removeFile = false);
+        // 将第 index0 个图像和第 index1 个图像在管理器中的顺序进行交换
         void swap(std::size_t index0, std::size_t index1);
-
-        /* 内容管理部分 */
-
-        bool checkIfValid() const;
-        bool checkIfValidIndex(std::size_t index) const;
-        bool checkIfImageExist(std::size_t index) const;
+        // 把新的图像添加到管理器里
+        void add(const fs::path &imagePath);
+        // 获取第 index 个图像的路径
+        // 未处理 index 不合法的情况
+        const fs::path &getImagePath(std::size_t index) const;
+        // 获取第 index 个图像的二进制路径
+        // 返回值类型为 std::unique_ptr<std::string>
+        // 如果图像路径失效（index不合法或者路径上文件不存在），则返回nullptr
         std::unique_ptr<std::string> getImageContent(std::size_t index) const;
+        // 清空管理器，扫描 srcPath 目录下的图像文件并添加到管理器内
+        void scanImageFiles(const fs::path &srcPath, bool add = false);
+
+        // 向 file 内写入类
+        bool write(std::ofstream &out) const;
+        // 从 file 内入读类
+        bool read(std::ifstream &file);
 
     private:
-        std::size_t m_scanImageFiles();
+        void m_copy(const fs::path &destPath, bool moveOldPath);
+        void m_move(const fs::path &destPath);
+        bool m_checkIndex(std::size_t i) const;
     };
 }
--- src/Img.cpp
+++ src/Img.cpp
@@ -7,47 +7,120 @@ using namespace book;
 /* ===== BEGIN ===== */
 // 构造函数
 
-ImagesManager::ImagesManager(const fs::path& path) {
-    if (!fs::exists(path)) return;
-    if (!fs::is_directory(path)) return;
-    m_curDir = fs::canonical(path);
-    m_scanImageFiles();
+ImagesManager::ImagesManager(const fs::path &path) {
+    scanImageFiles(path);
 }
 
-ImagesManager::ImagesManager(const fs::path& srcPath, const fs::path& destPath, bool removeOldFile, bool removeOldDirectory) {
-    if (!fs::exists(srcPath)) return ;
-    if (!fs::is_directory(srcPath)) return ;
-    if (fs::exists(destPath)) return ;
-    chdir(srcPath);
-    if (removeOldFile) move(destPath, removeOldDirectory);
-    else {
-        copy(destPath);
-        chdir(destPath);
-    }
-}
+ImagesManager::ImagesManager(const std::vector<fs::path> &images)
+: m_images(images) { }
 
-// 移动构造函数
-
-ImagesManager::ImagesManager(ImagesManager&& man) :
-    m_curDir(std::move(man.m_curDir)), m_imagesList(std::move(m_imagesList))
-{ }
+ImagesManager::ImagesManager(std::vector<fs::path> &&images)
+: m_images(std::move(images)) { }
 
 // 公有函数
 
-void ImagesManager::move(const fs::path& path, bool removeOldDirectory) {
-    copy(path);
-    if (removeOldDirectory) fs::remove_all(m_curDir);
-    else {
-        for (std::size_t i = 0; i < m_imagesList.size(); ++i) {
-            if (!checkIfImageExist(i)) continue;
-            const auto &img = m_imagesList.at(i);
-            fs::remove(m_curDir / img);
-        }
-    }
-    chdir(path);
+void ImagesManager::copy(const fs::path &destPath, bool moveOldPath) {
+    fs::create_directories(destPath);
+    m_copy(destPath, moveOldPath);
 }
 
-void ImagesManager::copy(const fs::path &path) {
+void ImagesManager::move(const fs::path &destPath) {
+    fs::create_directories(destPath);
+    m_move(destPath);
+}
+
+void ImagesManager::clear(bool removeFiles) {
+    if (removeFiles) {
+        for (const auto &path : m_images) {
+            fs::remove(path);
+        }
+    }
+    m_images.clear();
+}
+
+void ImagesManager::remove(std::size_t index, bool removeFile) {
+    if (!m_checkIndex(index)) return ;
+    if (removeFile) fs::remove(m_images.at(index));
+    for (auto i = m_images.begin() + index + 1; i != m_images.end(); ++i) {
+        *(i - 1) = *i;
+    }
+    m_images.pop_back();
+}
+
+void ImagesManager::swap(std::size_t index0, std::size_t index1) {
+    if (!m_checkIndex(index0) || !m_checkIndex(index1)) return ;
+    m_images.at(index0).swap(m_images.at(index1));
+}
+
+void ImagesManager::add(const fs::path &imagePath) {
+    if (!fs::exists(imagePath) || !fs::is_regular_file(imagePath)) return ;
+    fs::path path(fs::canonical(imagePath));
+    m_images.emplace_back(std::move(path));
+}
+
+const fs::path &ImagesManager::getImagePath(std::size_t index) const {
+    return m_images.at(index);
+}
+
+std::unique_ptr<std::string> ImagesManager::getImageContent(std::size_t index) const {
+    if (!m_checkIndex(index)) return nullptr;
+
+    const auto &path = m_images.at(index);
+    auto size = fs::file_size(path);
+    std::ifstream fin(path, std::ios::in | std::ios::binary);
+    if (fin.fail()) return nullptr;
+
+    std::unique_ptr<std::string> ret(new std::string(size, '\x00'));
+    fin.read(std::addressof((*ret)[0]), size); fin.close();
+    return ret;
+}
+
+void ImagesManager::scanImageFiles(const fs::path &srcPath, bool add) {
+    if (!fs::exists(srcPath) || !fs::is_directory(srcPath)) return ;
+
+    if (!add) m_images.clear();
+    for (auto &i : fs::directory_iterator(srcPath)) {
+        if (!i.is_regular_file()) continue;
+        auto &path = i.path();
+        for (auto type : IMG_TYPES) {
+            if (type != path.extension()) continue;
+            m_images.emplace_back(path);
+        }
+    }
+}
+
+bool ImagesManager::write(std::ofstream &out) const {
+    std::size_t size = m_images.size(), length;
+    out.write(reinterpret_cast<const char *>(&size), sizeof(size));
+    for (auto &path : m_images) {
+        auto &tmp = path.native();
+        std::string ret;
+        if constexpr (std::is_same_v<fs::path::string_type, std::wstring>) {
+            std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+            ret = converter.to_bytes(tmp);
+        } else if constexpr (std::is_same_v<fs::path::string_type, std::string>) {
+            // FIXIT TOMORROW
+            ret = tmp;
+        } else return false;
+        length = ret.length();
+        out.write(reinterpret_cast<const char *>(&length), sizeof(length));
+        out.write(ret.c_str(), ret.size());
+    }
+    return true;
+}
+
+bool ImagesManager::read(std::ifstream &in) {
+    std::size_t size, length;
+    std::string tmp;
+    in.read(reinterpret_cast<char *>(&size), sizeof(size));
+    for (std::size_t i = 0; i < size; ++i) {
+        in.read(reinterpret_cast<char *>(&length), sizeof(length));
+        if constexpr (std::is_same_v<fs::path::string_type, std::wstring>) {
+        } else if constexpr (std::is_same_v<fs::path::string_type, std::string>) {
+
+        } else return false;
+    }
+    return true;
 }
 
 // 私有函数
